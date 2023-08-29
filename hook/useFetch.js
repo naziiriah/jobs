b20775453349c198ad579f84098d0d3b4f770063
naziriiah <naziistry7@gmail.com>
2023-08-29 20:17:21 +0100

    done

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,51 +1,47 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const rapidapiKey = '2ecf899a9amshabcf05c96a08afap194ca0jsna66f157ef985'
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
 
-const useFetch  = (endpoint, query) => {
-    const [data, setData] = useState([]),
-        [isLoading, setIsLoading] = useState(false),
-        [error, setError] = useState(null),
-        options = {
-            method: 'GET',
-            url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-            params: {
-                ...query
-            },
-            headers: {
-                'X-RapidAPI-Key': rapidapiKey,
-                'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-            }
-      };
+  const fetchData = async () => {
+    setIsLoading(true);
 
-    const callEndpointFunction = async () => {
-        setIsLoading(true)
-        try {
-           const response =  await axios.request(options)
-           setData(response.data.data)
-           setIsLoading(false)
-        } catch (error) {
-            setError(error);
-            alert('There is an error')
-            setIsLoading(false)
-        }
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    useEffect(() => {
-        callEndpointFunction()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const reFetch = () => {
-        setIsLoading(true)
-        callEndpointFunction()
-    }
-    return {
-        data,
-        error,
-        isLoading
-    }
-}
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
 
 export default useFetch;
